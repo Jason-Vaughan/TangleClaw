18bc1832600272d73b52dd5759b7676a37cc0d5e
Jason-Vaughan <95194903+Jason-Vaughan@users.noreply.github.com>
2026-09-19 19:42:49 -0700

    fix(website): Update next.config.ts to allow Tailscale dev origins

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['cursatory.tail123678.ts.net'],
 };
 
 export default nextConfig;
